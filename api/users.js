7b8cc699121d5f741b2b915b4449e39e28e89698
precious-adeyinka <pflash90@gmail.com>
2020-10-30 00:47:25 +0100

    Feat: changed User Model require path in api/user.js file

--- api/users.js
+++ api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 //User Model
-const User = require('./models/User');
+const User = require('./models/user');
 
 //get all users
 router.get('/', (req, res) => {
